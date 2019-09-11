4a0ce632f00f713f667a41f8fc475d31ea0506df
mqiqe <mqiqe@163.com>
2019-09-11 11:53:26 +0800

    init

---
+++ handlers.go
@@ -0,0 +1,84 @@
+// Copyright 2018 Telefónica
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+)
+
+func receiveHandler(producer sarama.SyncProducer, serializer Serializer) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		httpRequestsTotal.Add(float64(1))
+		compressed, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			logrus.WithError(err).Error("couldn't read body")
+			return
+		}
+		// 发送信息到kafka
+		msg := &sarama.ProducerMessage{Topic: "metrics", Value: sarama.StringEncoder(compressed)}
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			logrus.Printf("FAILED to send message: %s\n", err)
+		} else {
+			logrus.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+		}
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			logrus.WithError(err).Error("couldn't produce message in kafka")
+			return
+		}
+
+		//
+		//reqBuf, err := snappy.Decode(nil, compressed)
+		//if err != nil {
+		//	c.AbortWithStatus(http.StatusBadRequest)
+		//	logrus.WithError(err).Error("couldn't decompress body")
+		//	return
+		//}
+		//
+		//var req prompb.WriteRequest
+		//if err := proto.Unmarshal(reqBuf, &req); err != nil {
+		//	c.AbortWithStatus(http.StatusBadRequest)
+		//	logrus.WithError(err).Error("couldn't unmarshal body")
+		//	return
+		//}
+
+		//metrics, err := processWriteRequest(&req)
+		//if err != nil {
+		//	c.AbortWithStatus(http.StatusInternalServerError)
+		//	logrus.WithError(err).Error("couldn't process write request")
+		//	return
+		//}
+
+		//for _, metric := range metrics {
+		//	err := producer.Produce(&kafka.Message{
+		//		TopicPartition: kafkaPartition,
+		//		Value:          metric,
+		//	}, nil)
+		//
+		//	if err != nil {
+		//		c.AbortWithStatus(http.StatusInternalServerError)
+		//		logrus.WithError(err).Error("couldn't produce message in kafka")
+		//		return
+		//	}
+		//}
+
+	}
+}
---
+++ main.go
@@ -0,0 +1,68 @@
+// Copyright 2018 Telefónica
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"github.com/containous/traefik/log"
+	"github.com/gin-gonic/contrib/ginrus"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+func main() {
+	log.Info("creating kafka producer")
+
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	client, err := sarama.NewClient([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalf("unable to create kafka client: %q", err)
+		panic(err)
+	}
+
+	producer, err := sarama.NewSyncProducerFromClient(client)
+	if err != nil {
+		log.Fatalf("unable to create kafka producer: %q", err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if err != nil {
+		logrus.WithError(err).Fatal("couldn't create kafka producer")
+	}
+
+	r := gin.New()
+
+	r.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true), gin.Recovery())
+
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	if basicAuth {
+		authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+			basicAuthUsername: basicAuthPassword,
+		}))
+		authorized.POST("/receive", receiveHandler(producer, serializer))
+	} else {
+		r.POST("/receive", receiveHandler(producer, serializer))
+	}
+
+	r.Run()
+}
